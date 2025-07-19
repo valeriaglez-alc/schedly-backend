234f2156699a3a97849ed6c7c5687c621222c45f
valeriaglez-alc <valeria.alcgo@gmail.com>
2025-07-19 13:41:40 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import db from './src/config/db.js';
+import dotenv from 'dotenv';
+import talleresRoute from './src/routes/talleresRoute.js';
+import usuarioRoute from './src/routes/usuarioRoute.js';
+import centrosCulturalesRoute from './src/routes/centrosCulturalesRoute.js';
+import asistenciasRoute from './src/routes/asistenciasRoute.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/api/talleres', talleresRoute);
+app.use('/api/usuarios', usuarioRoute);
+app.use('/api/centros', centrosCulturalesRoute);
+app.use('/api/asistencias', asistenciasRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
