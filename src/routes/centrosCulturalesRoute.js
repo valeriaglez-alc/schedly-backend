3abeb1c070674d1357cc56b5b6842a21f6afe948
valeriaglez-alc <valeria.alcgo@gmail.com>
2025-08-09 13:38:55 -0600

    Update centrosCulturalesRoute.js

--- src/routes/centrosCulturalesRoute.js
+++ src/routes/centrosCulturalesRoute.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { registrarCentroCultural } from '../controllers/centrosCulturalesController.js';
+import {
+  registrarCentroCultural,
+  listarCentrosCulturales,
+  obtenerCentro,
+  editarCentroCultural,
+  borrarCentroCultural
+} from '../controllers/centrosCulturalesController.js';
 
 const router = express.Router();
 
 router.post('/', registrarCentroCultural);
+router.get('/', listarCentrosCulturales);
+router.get('/:id', obtenerCentro);
+router.put('/:id', editarCentroCultural);
+router.delete('/:id', borrarCentroCultural);
 
 export default router;
