5002a60ebee64260ab9a86f8443884b31c2e4ac6
valeriaglez-alc <valeria.alcgo@gmail.com>
2025-08-09 13:37:17 -0600

    Update CentroCultural.js

--- src/models/CentroCultural.js
+++ src/models/CentroCultural.js
@@ -8,3 +8,31 @@ export const crearCentroCultural = async ({ nombre, direccion, descripcion, cont
   const [result] = await db.execute(query, [nombre, direccion, descripcion, contacto]);
   return result;
 };
+
+export const obtenerCentrosCulturales = async () => {
+  const query = `SELECT * FROM centros_culturales ORDER BY id DESC`;
+  const [rows] = await db.execute(query);
+  return rows;
+};
+
+export const obtenerCentroPorId = async (id) => {
+  const query = `SELECT * FROM centros_culturales WHERE id = ?`;
+  const [rows] = await db.execute(query, [id]);
+  return rows[0];
+};
+
+export const actualizarCentroCultural = async (id, { nombre, direccion, descripcion, contacto }) => {
+  const query = `
+    UPDATE centros_culturales
+    SET nombre = ?, direccion = ?, descripcion = ?, contacto = ?
+    WHERE id = ?
+  `;
+  const [result] = await db.execute(query, [nombre, direccion, descripcion, contacto, id]);
+  return result;
+};
+
+export const eliminarCentroCultural = async (id) => {
+  const query = `DELETE FROM centros_culturales WHERE id = ?`;
+  const [result] = await db.execute(query, [id]);
+  return result;
+};
